5170e8a981f6e3f986104b8266e2b058898eed16
Ilya Sidoruk <i.sidoruk03@gmail.com>
2025-01-25 23:27:22 +0300

    deploy: remove proxy from vite.config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,32 +1,32 @@
-import {defineConfig, loadEnv} from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import mkcert from 'vite-plugin-mkcert'
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => {
+export default defineConfig(() => {
   // Загружаем переменные окружения в зависимости от режима (development/production)
-  const env = loadEnv(mode, process.cwd(), '')
+  // const env = loadEnv(mode, process.cwd(), '')
   
   return {
     plugins: [
       react(),
       mkcert()
     ],
-    server: {
-      proxy: {
-        '/socket.io': {
-          target: env.VITE_API_URL || 'https://localhost:3001',
-          ws: true,
-          secure: false,
-          changeOrigin: true
-        },
-        '/api': {
-          target: env.VITE_API_URL || 'https://localhost:3001',
-          changeOrigin: true,
-          secure: false,
-          rewrite: (path) => path.replace(/^\/api/, '')
-        }
-      }
-    },
+    // server: {
+    //   proxy: {
+    //     '/socket.io': {
+    //       target: env.VITE_API_URL || 'https://localhost:3001',
+    //       ws: true,
+    //       secure: false,
+    //       changeOrigin: true
+    //     },
+    //     '/api': {
+    //       target: env.VITE_API_URL || 'https://localhost:3001',
+    //       changeOrigin: true,
+    //       secure: false,
+    //       rewrite: (path) => path.replace(/^\/api/, '')
+    //     }
+    //   }
+    // },
   }
 })
